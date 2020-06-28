25a82e26edc0f1ae34e2348f480098d12ea4479f
Larry_A <larryagbana@gmail.com>
2020-06-28 14:48:47 -0400

    improve web performance, resized images

--- client/src/components/ImageCard/index.js
+++ client/src/components/ImageCard/index.js
@@ -1,16 +1,14 @@
 // Import react
 import React from 'react'
 // Import styling
-import './style.css'
+import './ImageCard.module.css'
 
+export default function ImageCard (props) {
+  const { src, alt, onClick } = props
 
-export default function ImageCard(props) {
-    const { src, alt, onClick } = props
-
-    return (
-            <div className='img-block'>
-              <img src={src} alt={alt} onClick={onClick} />
-            </div>
-    )
-
+  return (
+    <div className='img-block'>
+      <img src={src} alt={alt} onClick={onClick} />
+    </div>
+  )
 }
