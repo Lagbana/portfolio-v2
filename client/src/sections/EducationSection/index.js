a4aedf9557ce8bd2a90729857051522e3be9d386
Larry_A <larryagbana@gmail.com>
2020-06-27 23:49:08 -0400

    sections components

---
+++ client/src/sections/EducationSection/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function EducationSection(props) {
+  const {id} = props
+  return (
+    <section
+      className='site-layout-background mainview'
+      id={id}
+      style={{
+        backgroundColor: 'purple',
+        color: 'white',
+        padding: '2vw 2vw 2vw 15vw',
+        minHeight: '100vh'
+      }}
+    >
+      <p>
+        With Reach Router, any component that's a direct child of the router
+        receives a location prop that describes the user's current location
+        within the app. In my case, the header navigation is a child of a page
+        layout component. Rather than passing location through as a prop, I've
+        used the Location component. This provides a child render prop that has
+        access to the user's location.
+      </p>
+    </section>
+  )
+}
---
+++ client/src/sections/index.js
@@ -0,0 +1,6 @@
+export * from './AboutSection'
+export * from './BlogSection'
+export * from './ContactSection'
+export * from './EducationSection'
+export * from './ExperienceSection'
+export * from './ProjectsSection'
