94fde40dcb1be085cce73fc31f3944e2d2f7e255
Larry_A <larryagbana@gmail.com>
2020-07-17 12:25:10 -0400

    remove docker, replace with procfile

---
+++ client/build/precache-manifest.f4d95900df02ded23585e77fba7fb7af.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43d8a38d2453210698de210c64b8404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb887824e10755d1941c",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "ea20571566412032c56c",
+    "url": "/static/css/main.d6b44c7a.chunk.css"
+  },
+  {
+    "revision": "bb887824e10755d1941c",
+    "url": "/static/js/2.34599fca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.34599fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea20571566412032c56c",
+    "url": "/static/js/main.34662517.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "89d6e40d16e9cb8a40a4707507c8b13a",
+    "url": "/static/media/LarryAgbana.89d6e40d.pdf"
+  },
+  {
+    "revision": "a79fbfaee44e73253adfd8fd04e3cf90",
+    "url": "/static/media/covid-19.a79fbfae.PNG"
+  },
+  {
+    "revision": "eb2da3a74bee3163e9b6337575d399e2",
+    "url": "/static/media/good-samaritan.eb2da3a7.PNG"
+  },
+  {
+    "revision": "f84b3170e1b0df6ea93faf1ba274cbae",
+    "url": "/static/media/hr-manager.f84b3170.PNG"
+  },
+  {
+    "revision": "524a3ee70f986618aaf8ec65ede25973",
+    "url": "/static/media/larry.524a3ee7.png"
+  },
+  {
+    "revision": "cb670a2cbf036c5d07c3aa27a4039985",
+    "url": "/static/media/travelbook.cb670a2c.PNG"
+  },
+  {
+    "revision": "fd4cc27055b5069a92b6a539e32036d4",
+    "url": "/static/media/weatherdashboard.fd4cc270.PNG"
+  },
+  {
+    "revision": "f9c3b6c28cf8e1d0a9638b0616392d6a",
+    "url": "/static/media/work-scheduler.f9c3b6c2.png"
+  }
+]);
